f69f02a9827fe229a276b2a5117258fd938486be
Tohidul29 <tohidswfu@gmail.com>
2022-05-25 01:33:13 +0600

    single data loaded using id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -18,11 +18,20 @@ async function run(){
         await client.connect();
         const toolsCollection = client.db('Nucleus_of_PC').collection('tools');
 
+        //get all tools:
         app.get('/tools', async(req, res)=>{
             const query = {};
             const cursor = toolsCollection.find(query);
             const tools = await cursor.toArray();
             res.send(tools);
+        })
+
+        // load single tool
+        app.get('/tools/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const tool = await toolsCollection.find(query);
+            res.send(tool);
         })
     }
     finally{
