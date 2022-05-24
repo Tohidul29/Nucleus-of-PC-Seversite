fabde3758beb0b1b4f87f6384e161078c2369dda
Tohidul29 <tohidswfu@gmail.com>
2022-05-25 05:11:40 +0600

    code update

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ async function run(){
         app.get('/tools/:id', async(req, res)=>{
             const id = req.params.id;
             const query = {_id: ObjectId(id)};
-            const tool = await toolsCollection.find(query);
+            const tool = await toolsCollection.findOne(query);
             res.send(tool);
         })
     }
