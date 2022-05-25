459a19cf1a8ef3ae6e03a6066f9de4081e4de012
Tohidul29 <tohidswfu@gmail.com>
2022-05-26 04:34:11 +0600

    user method added

--- index.js
+++ index.js
@@ -13,45 +13,58 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
-async function run(){
-    try{
+async function run() {
+    try {
         await client.connect();
         const toolsCollection = client.db('Nucleus_of_PC').collection('tools');
         const purchaseCollection = client.db('Nucleus_of_PC').collection('purchase');
+        const userCollection = client.db('Nucleus_of_PC').collection('users');
 
         //get all tools:
-        app.get('/tools', async(req, res)=>{
+        app.get('/tools', async (req, res) => {
             const query = {};
             const cursor = toolsCollection.find(query);
             const tools = await cursor.toArray();
             res.send(tools);
         })
 
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            res.send(result);
+        })
+
         // load single tool
-        app.get('/tools/:id', async(req, res)=>{
+        app.get('/tools/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const tool = await toolsCollection.findOne(query);
             res.send(tool);
         });
 
         //for submit users purchase product:
-        app.post('/purchase', async(req, res)=>{
+        app.post('/purchase', async (req, res) => {
             const purchase = req.body;
             const output = await purchaseCollection.insertOne(purchase);
             res.send(output);
         })
 
         //get users product details:
-        app.get('/purchase', async(req, res) =>{
+        app.get('/purchase', async (req, res) => {
             const email = req.query.buyerEmail;
-            const query = {buyerEmail: email};
-            const purchase = await purchaseCollection.find(query).toArray();
-            res.send(purchase);
+            const query = { buyerEmail: email };
+            const order = await purchaseCollection.find(query).toArray();
+            res.send(order);
         })
 
     }
-    finally{
+    finally {
 
     }
 }
@@ -59,10 +72,10 @@ async function run(){
 run().catch(console.dir);
 
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send('Nucleus of PC is running');
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`listing to the port ${port}`);
 })
